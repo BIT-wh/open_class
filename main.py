2dfd066daa99f0053718a903bd16c9aecf523bcb
BIT-wh <1094020842@qq.com>
2025-12-27 10:27:39 +0800

    debug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import float_bubble
-import gravity_bubble
+from float_bubble.main_float_bubble import app
+# from gravity_bubble.main_gravity_bubble import app
 
-if __name__ == '__main__':
-    float_bubble.main()
-    # gravity_bubble.main()
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
